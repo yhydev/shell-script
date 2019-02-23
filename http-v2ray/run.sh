db88b51e9ff07cdf03b5540afb84ce684a2980ff
yhydev <ubuntu@ip-10-23-8-56.cn-north-1.compute.internal>
2019-02-23 19:27:37 +0000

    update http-v2ray/run.sh

--- http-v2ray/run.sh
+++ http-v2ray/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 
-wget -O 
+wget -O config.json https://raw.githubusercontent.com/yhydev/shell-script/master/http-v2ray/config.json 
 
 docker run -d -p 80:8080/tcp -p 80:8080/udp --name http-v2ray -v /home/ubuntu/config.json:/etc/v2ray/config.json v2ray/official
