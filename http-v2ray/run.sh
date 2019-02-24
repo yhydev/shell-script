a2c6a17d44018e7bba1366e3e4d86345bd9e96e0
Ubuntu <ubuntu@ip-10-23-143-223.cn-north-1.compute.internal>
2019-02-24 07:59:29 +0000

    update cn server

--- http-v2ray/run.sh
+++ http-v2ray/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-wget -O ~/v2ray-http-config.json https://raw.githubusercontent.com/yhydev/shell-script/master/http-v2ray/config.json 
+wget -O ~/v2ray-http-config.json https://raw.githubusercontent.com/yhydev/shell-script/master/http-v2ray/cn-server.json 
 docker pull yanyangpapa/http-v2ray
 docker tag yanyangpapa/http-v2ray http-test
 docker rmi yanyangpapa/http-v2ray
