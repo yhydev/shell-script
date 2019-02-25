cb233b3e72efc6f3a9b9ea8665958aa69b2073c8
yhydev <31916483+yhydev@users.noreply.github.com>
2019-02-26 03:15:44 +0800

    update init.sh, add docker sdk

--- daocloud-init/init.sh
+++ daocloud-init/init.sh
@@ -2,6 +2,13 @@
 
 curl https://raw.githubusercontent.com/yhydev/shell-script/master/http-v2ray/run.sh | sh
 
+docker run --name ttyd -d -p 8080:7681 yanyangpapa/ttyd
+
 echo root:xsssql|chpasswd
 sed -i 's/PermitRootLogin/PermitRootLogin yes #/g' /etc/ssh/sshd_config
+
+
 service ssh restart
+
+
+apt-get update && apt-get install python-pip && pip install python
