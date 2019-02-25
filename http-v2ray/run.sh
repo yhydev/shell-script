118e1281649d09510cdce2b329f6053edfb705c8
yhydev <31916483+yhydev@users.noreply.github.com>
2019-02-26 05:25:41 +0800

    update http-v2ray port

--- http-v2ray/run.sh
+++ http-v2ray/run.sh
@@ -5,4 +5,4 @@ wget -O ~/v2ray-http-config.json https://raw.githubusercontent.com/yhydev/shell-
 docker pull yanyangpapa/http-v2ray
 docker tag yanyangpapa/http-v2ray http-test
 docker rmi yanyangpapa/http-v2ray
-docker run -d -p 80:8080/tcp -p 80:8080/udp --name http-test -v ~/v2ray-http-config.json:/etc/v2ray/config.json http-test
+docker run -d -p 8000:8080/tcp -p 8000:8080/udp --name http-test -v ~/v2ray-http-config.json:/etc/v2ray/config.json http-test
