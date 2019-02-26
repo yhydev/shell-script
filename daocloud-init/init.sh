c8a8f9db5f617d903722b080ab45c8b29330014d
yhydev <31916483+yhydev@users.noreply.github.com>
2019-02-27 02:10:48 +0800

    delete tyyd

--- daocloud-init/init.sh
+++ daocloud-init/init.sh
@@ -2,11 +2,8 @@
 
 curl https://raw.githubusercontent.com/yhydev/shell-script/master/http-v2ray/run.sh | sh
 
-docker run --name ttyd -d -p 8080:7681 yanyangpapa/ttyd
-
 echo root:xsssql|chpasswd
 sed -i 's/PermitRootLogin/PermitRootLogin yes #/g' /etc/ssh/sshd_config
-
 
 service ssh restart
 
