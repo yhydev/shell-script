927a84edc6f141de5220b89a0fd4c6fc8d66ebb8
yhydev <31916483+yhydev@users.noreply.github.com>
2019-04-09 15:02:12 +0800

    Update init.sh
    remove share-linux code

--- daocloud-init/init.sh
+++ daocloud-init/init.sh
@@ -7,7 +7,7 @@ sed -i 's/PermitRootLogin/PermitRootLogin yes #/g' /etc/ssh/sshd_config
 
 service ssh restart
 
-docker pull ubuntu
-docker run -d -p 80:80 -v /var/run/docker.sock:/var/run/docker.sock --name share-linux  yanyangpapa/share-linux:usable
+#docker pull ubuntu
+#docker run -d -p 80:80 -v /var/run/docker.sock:/var/run/docker.sock --name share-linux  yanyangpapa/share-linux:usable
 
 #curl  https://raw.githubusercontent.com/yhydev/shell-script/master/ttyd/ubuntu-16.04-install.sh | sh
